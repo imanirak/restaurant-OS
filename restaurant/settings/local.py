32104ee0df3e317a6f0a3d2d38e565cc925e4e44
Karina Garces ✨ <karinadgarces@gmail.com>
2022-12-09 16:39:59 -0500

    local settings

---
+++ restaurant/settings/local.py
@@ -0,0 +1,17 @@
+import os
+from .base import *
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+ALLOWED_HOSTS = ["*"]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
